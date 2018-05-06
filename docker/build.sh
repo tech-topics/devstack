331cbf7ab19d7acf9f8efdae8e61cf24a6b5c4f3
tech-topics <tech-topics@users.noreply.github.com>
2018-05-05 19:22:52 -0700

    Updated Dockerfile to create the devuser user

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 ## ============================================
-##	Project:	tomcat-svr
-##	File:		build.sh
+##	Project:	devstack
+##	File:		docker/build.sh
 ##	Author:		Donald Raikes
-##	Date:		04/19/2018
+##	Date:		05/05/2018
 ##
-##	Script to build the dnraikes/tomcat-svr image.
+##	Script to build the devstack image.
 ## ============================================
-docker build --force-rm=true --compress \
+USERID=`id -u`
+GID=`id -g`
+docker build --force-rm=true --compress -q \
 	--build-arg HTTP_PROXY=http://www-proxy.us.oracle.com:80 \
 	--build-arg HTTPS_PROXY=http://www-proxy.us.oracle.com:80 \
 	--build-arg http_proxy=http://www-proxy.us.oracle.com:80 \
 	--build-arg https_proxy=http://www-proxy.us.oracle.com:80 \
+	--build-arg USERID=$USERID \
+	--build-arg GROUPID=$GID \
 	-t raikessecurity/devstack .
--- docker/run.sh
+++ docker/run.sh
@@ -3,5 +3,7 @@ docker kill devstack
 docker run --name devstack --rm -it \
 	-e http_proxy=http://www-proxy.us.oracle.com:80 \
 	-e https_proxy=http://www-proxy.us.oracle.com:80 \
+	-e HTTP_PROXY=http://www-proxy.us.oracle.com:80 \
+	-e HTTPS_PROXY=http://www-proxy.us.oracle.com:80 \
 	-v /tmp:/tmp \
 	raikessecurity/devstack:latest bash
